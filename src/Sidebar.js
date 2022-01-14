cef982f5ce79ce4cc3028d42e1a0bcb4765523bc
anishkumar5478 <anishkumar861999@gmail.com>
2022-01-14 09:33:08 +0530

    V2 Commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,29 @@
 import React from "react";
 import "./sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>this is side bar</h1>
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+      <SidebarOption title="tere kasam hip" />
+      <SidebarOption title="tere kasam hip" />
+      <SidebarOption title="tere kasam hip" />
+      <SidebarOption title="tere kasam hip" />
+      <SidebarOption title="tere kasam hip" />
     </div>
   );
 }
